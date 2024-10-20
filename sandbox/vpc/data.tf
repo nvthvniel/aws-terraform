e83fab303a6336ea2148877e12c3b9d66826e23c
nathaniel <nathanielstevens@hotmail.com>
2024-10-20 16:47:12 +0100

    fix: workflow file

--- sandbox/vpc/data.tf
+++ sandbox/vpc/data.tf
@@ -3,8 +3,8 @@ data "terraform_remote_state" "s3" {
 
   config = {
     dynamodb_table = "terraform-remote-state-445567079474"
-    bucket = "terraform-remote-state-445567079474"
-    key    = "sandbox/vpc/terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "terraform-remote-state-445567079474"
+    key            = "sandbox/vpc/terraform.tfstate"
+    region         = "eu-west-2"
   }
 }
