6420a06dce15b9966b93995ffa6c42fba162829c
nathaniel <nathanielstevens@hotmail.com>
2024-10-20 16:54:20 +0100

    fix: backend config

--- sandbox/vpc/versions.tf
+++ sandbox/vpc/versions.tf
@@ -5,4 +5,11 @@ terraform {
       version = "~> 5.72"
     }
   }
+
+  backend "s3" {
+    bucket         = "terraform-remote-state-445567079474"
+    key            = "sandbox/vpc/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-remote-state-445567079474"
+  }
 }
